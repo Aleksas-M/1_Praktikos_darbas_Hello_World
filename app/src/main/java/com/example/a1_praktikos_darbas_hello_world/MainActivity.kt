bda2b7593aabd66a306ea04ac7fc973da8a7c611
Aleksas M <aleksas.motiejunas60@gmail.com>
2025-09-22 22:09:11 +0300

    Add second button to change TextView text color

--- app/src/main/java/com/example/a1_praktikos_darbas_hello_world/MainActivity.kt
+++ app/src/main/java/com/example/a1_praktikos_darbas_hello_world/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.a1_praktikos_darbas_hello_world
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val tv = findViewById<TextView>(R.id.tvMessage)
-        val btn = findViewById<Button>(R.id.btnSetText)
+        val btnSetText = findViewById<Button>(R.id.btnSetText)
+        val btnChangeTextColor = findViewById<Button>(R.id.btnChangeTextColor)
 
-        btn.setOnClickListener {
+        btnSetText.setOnClickListener {
             tv.text = "Paspaudei mygtuką!"
+        }
+
+        btnChangeTextColor.setOnClickListener {
+            val isRed = tv.currentTextColor == Color.BLUE
+            tv.setTextColor(if (isRed) Color.BLACK else Color.BLUE)
         }
     }
 }
